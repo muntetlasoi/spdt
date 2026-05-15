7285a5b8602d2cca73dbe321b3a5267bf151a2b2
muntetlasoi <muntetlasoi@gmail.com>
2026-05-15 19:56:20 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,26 @@
-
+const products = [
+  {
+    id: 1,
+    name: "Hennessy",
+    price: 6500,
+    stock: 12,
+    image: "https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b",
+    description: "Premium cognac, smooth and rich."
+  },
+  {
+    id: 2,
+    name: "Jameson",
+    price: 3200,
+    stock: 8,
+    image: "https://images.unsplash.com/photo-1569529465841-dfecdab7503b",
+    description: "Smooth Irish whiskey."
+  },
+  {
+    id: 3,
+    name: "Smirnoff Vodka",
+    price: 2100,
+    stock: 0,
+    image: "https://images.unsplash.com/photo-1516594915697-87eb3b1c14ea",
+    description: "Classic clean vodka."
+  }
+];
