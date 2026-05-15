857c3eb83ee09b7db0e9e5d25e412830f71c3335
muntetlasoi <muntetlasoi@gmail.com>
2026-05-15 19:59:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+const container = document.getElementById("products");
+
+function renderProducts(){
+  container.innerHTML = "";
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.image}" onclick="view(${p.id})">
+        <h3>${p.name}</h3>
+        <p>KSh ${p.price}</p>
+        <p>${p.stock > 0 ? p.stock + " left" : "OUT OF STOCK"}</p>
+
+        <button onclick="addToCart(${p.id})" ${p.stock===0?'disabled':''}>
+          Add to Cart
+        </button>
+      </div>
+    `;
+  });
+}
+
+function addToCart(id){
+  const item = products.find(p=>p.id===id);
+  cart.push(item);
+  localStorage.setItem("cart",JSON.stringify(cart));
+  alert("Added to basket");
+}
+
+function view(id){
+  localStorage.setItem("selected",id);
+  window.location.href = "product.html";
+}
+
+renderProducts();
